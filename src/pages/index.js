b5ec10a29b7cfc20e8dbfc8e72b9386582c4500e
lichang <2217024156@qq.com>
2021-12-24 10:29:38 +0800

    lichang first

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Home</title>
+      <h1>首页内容</h1>
+    </main>
+  )
+}
+
+export default IndexPage
